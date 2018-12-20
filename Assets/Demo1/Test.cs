37cd4de14b6d83ed7dafbd37dadeaa13759c4170
chenweichao <chen15270994267@gmail.com>
2018-12-20 11:35:29 +0800

    第一次上传

---
+++ Assets/Demo1/Test.cs
@@ -0,0 +1,80 @@
+﻿
+#region 版权信息
+/*
+ * -----------------------------------------------------------
+ *  Copyright (c) KeJun All rights reserved.
+ * -----------------------------------------------------------
+ *		描述: 
+ *      创建者：#DEVELOPERNAME#
+ *      创建时间: #CREATIONDATE#
+ *  
+ */
+#endregion
+
+
+using UnityEngine;
+
+namespace Demo1
+{
+    public class Test : MonoBehaviour
+    {
+        #region Fields
+        public float mSize;
+        public ParticleSystem mPs;
+        public ParticleSystem.MainModule mModule;
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Unity Messages
+        //    void Awake()
+        //    {
+        //
+        //    }
+        //    void OnEnable()
+        //    {
+        //
+        //    }
+        //
+        void Start()
+        {
+            mModule = mPs.main;
+        }
+        //    
+        //    void Update() 
+        //    {
+        //    
+        //    }
+        //
+        //    void OnDisable()
+        //    {
+        //
+        //    }
+        //
+        //    void OnDestroy()
+        //    {
+        //
+        //    }
+
+        #endregion
+
+        #region Private Methods
+        [ContextMenu("SetSize")]
+        private void SetSize()
+        {
+            /*
+            var temp = mPs.main;
+            temp.startSize = mSize;
+            */
+
+            mModule.startSize = mSize;
+        }
+        #endregion
+
+        #region Protected & Public Methods
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ParticalSystemDemo
+
+Demo1 示例--动态设置粒子大小
